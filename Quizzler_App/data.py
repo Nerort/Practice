3d228589ac75ad20ca4cb2658fb923f72ad55d5b
Nerort <59502452+Nerort@users.noreply.github.com>
2022-06-30 19:20:34 +0300

    Add files via upload

---
+++ Quizzler_App/data.py
@@ -0,0 +1,10 @@
+import requests
+
+parameters = {
+    'amount': 10,
+    'type': 'boolean',
+}
+
+responce = requests.get('https://opentdb.com/api.php', params=parameters)
+responce.raise_for_status()
+question_data = responce.json()['results']
